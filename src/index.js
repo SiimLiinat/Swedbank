3535a7b1ac26f9a1de89d1b0e0b9a893610911e9
SiimLiinat <siimliinat@gmail.com>
2022-01-25 23:14:08 +0200

    Finished intro.

--- src/index.js
+++ src/index.js
@@ -1,88 +1,86 @@
 let currentTab = 0; // Current tab is set to be the first tab (0)
-showTab(currentTab); // Display the current tab
+showTab(); // Display the current tab
 
 let answers = {};
 
-function showTab(n) {
-    // This function will display the specified tab of the form ...
-    let x = document.getElementsByClassName("tab");
-    x[n].style.display = "block";
-    if (x[n].id === "summary") {
+function showTab() {
+    // TODO: All functions get tabs. Maybe it should be a separate function?
+    let tabs = document.getElementsByClassName("tab");
+    tabs[currentTab].style.display = "block";
+    if (tabs[currentTab].id === "summary") {
         for (let i = 0; i < 5; i++) {
             const answer = document.getElementById("answer" + (i + 1));
             answer.innerText = answers[i];
         }
     }
     // ... and fix the Previous/Next buttons:
-    if (n === 0) {
+    if (currentTab === 0) {
         document.getElementById("prevBtn").style.display = "none";
     } else {
         document.getElementById("prevBtn").style.display = "inline";
     }
-    if (n === x.length - 1) {
+    if (currentTab === tabs.length - 1) {
         document.getElementById("nextBtn").innerHTML = "Submit";
     } else {
         document.getElementById("nextBtn").innerHTML = "Next";
     }
     // ... and run a function that displays the correct step indicator:
-    if (currentTab !== 5) fixStepIndicator(n)
+    if (currentTab !== 5) fixStepIndicator(currentTab)
 }
 
-function nextPrev(n) {
+function nextPrev(change) {
     // This function will figure out which tab to display
-    let x = document.getElementsByClassName("tab");
+    let tabs = document.getElementsByClassName("tab");
     // Exit the function if any field in the current tab is invalid:
-    if (currentTab !== 5) if (n === 1 && !validateForm()) return false;
+    if (currentTab !== 5) if (change === 1 && !validateForm()) return false;
     // Hide the current tab:
-    x[currentTab].style.display = "none";
+    tabs[currentTab].style.display = "none";
     // Increase or decrease the current tab by 1:
-    currentTab = currentTab + n;
+    currentTab = currentTab + change;
 
     // if you have reached the end of the form... :
-    if (currentTab >= x.length) {
+    if (currentTab >= tabs.length) {
         //...the form gets submitted:
         submitForm()
         return false;
     }
     // Otherwise, display the correct tab:
-    showTab(currentTab);
+    showTab();
 }
 
 function validateForm() {
-    // This function deals with validation of the form fields
-    let x, y, i, valid = true;
-    x = document.getElementsByClassName("tab");
-    y = x[currentTab].querySelectorAll("input, textarea, select");
-    // A loop that checks every input field in the current tab:
+    let tabs = document.getElementsByClassName("tab");
+    let input = tabs[currentTab].querySelectorAll("input, textarea, select");
     // TODO: Should collecting answers be a separate thing at the end? Since the answers are always there, just hidden.
     let answer = "";
-    if (y[0].type === "checkbox") {
-        let value = y[0].checked;
-        if (!value) valid = false;
-        else answer = "True";
-    } else if (y[0].type === "radio") {
+    let valid = true;
+    if (input[0].type === "radio") {
         valid = false;
-        y.forEach(radio => {
+        input[0].className += " error"
+        input.forEach(radio => {
             if (radio.checked) {
                 answer = radio.value;
                 valid = true;
+                input[0].className = "";
             }
         });
-    } else if (y[0].tagName.toLowerCase() === "select") {
-        answer = y[0].value;
+    } else if (input[0].tagName.toLowerCase() === "select") {
+        input[0].className.replace(" error", "");
+        answer = input[0].value;
+        if (answer === "null") {
+            valid = false;
+            input[0].className += " error"
+        }
     }
     else {
-        for (i = 0; i < y.length; i++) {
-            // If a field is empty...
-            answer = y[i].value;
-            if (y[i].value === "") {
-                // add an "invalid" class to the field:
-                y[i].className += " invalid";
+        for (let i = 0; i < input.length; i++) {
+            answer = input[i].value;
+            if (input[i].value === "") {
+                input[i].className += " error";
                 valid = false;
             }
         }
     }
-    // If the valid status is true, mark the step as finished and valid:
     if (valid) {
         let step = document.getElementsByClassName("step")[currentTab];
         step.className += " steps-completed";
@@ -91,13 +89,13 @@ function validateForm() {
         stepToComplete.innerHTML = "check";
         answers[currentTab] = answer.trim();
     }
-    return valid; // return the valid status
+    return valid;
 }
 
 function fixStepIndicator(n) {
     // This function removes the "active" class of all steps...
-    let i, x = document.getElementsByClassName("step");
-    for (i = 0; i < x.length; i++) {
+    let x = document.getElementsByClassName("step");
+    for (let i = 0; i < x.length; i++) {
         x[i].className = x[i].className.replace(" steps-ongoing steps-selected", "");
         for (let node of x[i].childNodes) {
             if (i !== n) {
@@ -107,12 +105,6 @@ function fixStepIndicator(n) {
     }
     //... and adds the "active" class to the current step:
     x[n].className += " steps-ongoing steps-selected";
-    // hides steps-sub-title from non-active tabs
-    for (let node of x[n].childNodes) {
-        if (node.className === 'steps-sub-title' && node.style.display === 'none') {
-            node.style.display = "inline"
-        }
-    }
 }
 
 function submitForm() {
